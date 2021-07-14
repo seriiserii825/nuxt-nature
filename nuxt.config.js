a37af7ab87a9f693c2807f564c1a2557bd75d71d
serii <seriiburduja@gmail.com>
2021-07-14 10:00:46 +0300

    server

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,49 +1,52 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+  server: {
+    port: process.env.PORT,
+    host: "localhost"
+  },
+  env: {
+    baseUrl: process.env.BASE_URL
+  },
   head: {
-    title: 'nuxt-nature',
+    title: "nuxt-nature",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en"
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
-
+  router: { prefetchLinks: false },
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    'element-ui/lib/theme-chalk/index.css'
-  ],
+  css: ["element-ui/lib/theme-chalk/index.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    '@/plugins/element-ui'
-  ],
+  plugins: ["@/plugins/element-ui"],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
+    "@nuxt/loading-screen"
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  axios: { proxy: true },
+  proxy: { "/api/": process.env.BASE_URL },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     transpile: [/^element-ui/],
+    extractCSS: true
   }
-}
+};
---
+++ server/index.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+
+// Подключаем бэкенд на Express.
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Подключаем Mongoose и делаем коннект к базе данных.
+// Прописываем стандартные настройки Mongoose.
+// const mongoose = require("mongoose");
+// mongoose.Schema.Types.Boolean.convertToFalse.add("");
+// mongoose.connect(`mongodb://localhost/${process.env.DATABASE}`, {
+//   useCreateIndex: true,
+//   useNewUrlParser: true,
+//   useFindAndModify: false,
+//   useUnifiedTopology: true,
+// });
+
+// Подключаем маршруты для управления моделью Page.
+const homeRoutes = require("./routes/home.router");
+app.use("/api/home", homeRoutes);
+
+// Подключаем Nuxt в режиме nuxt.render. В этом примере нет отдельного процесса с Nuxt.
+// Nuxt работает в качестве middleware для Express без своего сервера на Connect.
+const { loadNuxt, build } = require("nuxt");
+const isDev = process.env.NODE_ENV !== "production";
+async function start() {
+  const nuxt = await loadNuxt(isDev ? "dev" : "start");
+  app.use(nuxt.render);
+  if (isDev) {
+    build(nuxt);
+  }
+  app.listen(process.env.PORT);
+}
+
+// Запуск приложения.
+start();
